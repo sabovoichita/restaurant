6ac5e32657050ef13928ed136339e896f0744142
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-08 10:31:41 +0200

    added class name properties for StyleGuide

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import StyleGuide from "@/components/ui/StyleGuide";
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
       <StyleGuide />
     </main>
   );
