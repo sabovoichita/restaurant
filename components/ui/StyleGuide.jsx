e3710688709ea7b4bcca18d22ce45ffca68943f8
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-08 11:51:33 +0200

    h1,   h2,   h3, p text styling

--- components/ui/StyleGuide.jsx
+++ components/ui/StyleGuide.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const StyleGuide = () => {
-  return <div>StyleGuide</div>;
+  return (
+    <>
+      {/*typography*/}
+      <div className="flex flex-col gap-y-4 p-24">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>
+          {" "}
+          some random text here some random text heresome random text heresome
+          random text here some random text here some random text here some
+          random text here some random text here
+        </p>
+      </div>
+    </>
+  );
 };
 export default StyleGuide;
